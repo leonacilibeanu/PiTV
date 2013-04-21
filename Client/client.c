b07da08066f54afa945e1337c3de0b6d083b1ce0
leonacilibeanu <leona.cilibeanu@gmail.com>
2013-04-21 10:04:20 +0300

    hackathon

--- Client/client.c
+++ Client/client.c
@@ -15,66 +15,27 @@
 int main(int argc, char *argv[])
 {
 	
-      int sockfd, newsockfd, portno, cont = 1, serverfd, pid;
-      char nume[50], buf[BUFLEN], buf1[BUFLEN];
-      struct sockaddr_in serv_addr, cli_addr, my_addr;
-      int n, i, j, k, dim_fis, ok;
+      int portno, serverfd, pid, streamno, f1;
+      char buf[BUFLEN], buf1[BUFLEN];
+      struct sockaddr_in serv_addr;
+      int i, k, n, fdmax;
       long dim;
-      char *args[3];
+      char *args[4];
+      fd_set read_fds;
+      fd_set tmp_fds;
       
-      int f, f1, f2;
-      int fdmax;
       int f_num = 0;
       char *name[2] = {"test1.wav", "test2.wav"};
       
-      if (argc < 2) {
+      if (argc < 3) {
         fprintf(stderr,"Usage : %s addr_server port\n", argv[0]);
         exit(1);
       }
          
 	    portno = atoi(argv[2]);
-
-	    //conectare la server
-	    serverfd = socket(AF_INET, SOCK_STREAM, 0);
-         	if (serverfd < 0) 
-            	error("Nu s-a putut deschide soketul");
-	    memset((char *) &serv_addr, 0, sizeof(serv_addr));
-	    serv_addr.sin_family = AF_INET;
-	    serv_addr.sin_port = htons(atoi(argv[2]));
-	    inet_aton(argv[1], &serv_addr.sin_addr);
-	    if (connect(serverfd,(struct sockaddr*) &serv_addr,sizeof(serv_addr)) < 0) 
-		    error("Eroare la conectare");
-		
-	   /* if ((k = recv(serverfd, buf, BUFLEN, 0)) <= 0)
-		      printf("S-a inchis serverul vietii!!!!\n");
-
-		  i = 0;
-		  
-		  printf("Got %d bytes and Got dim %ld\n", k, ((long *) buf)[0]);
-
-      //write initial file		
-		  dim = ((long *) buf)[0];
-
-		  f1 = open(name[f_num], O_CREAT | O_TRUNC | O_WRONLY, 0666);
-		  if (f1 < 0)
-		    exit(1);
-		  
-		  while (i < dim) {
-		    k = recv(serverfd, buf1, MIN(BUFLEN, dim - i), 0);
-        printf("Got %d and total is %d and min is %i\n", k, i + k, MIN(BUFLEN, dim - i));
-		    if (k <= 0)
-		      exit(1);
-				
-        
-			  n = write(f1, buf1, k);
-			  if (n < 0)
-				  exit(1);
-				  
-				i += k;
-		  }
-			
-		  close(f1);*/
-      f_num = (f_num + 1) % 2;
+      streamno = atoi(argv[3]);
+      if (streamno != 0 && streamno != 1) 
+        streamno = 0;
       
       pid = fork();
       
@@ -83,21 +44,75 @@ int main(int argc, char *argv[])
       
       if (pid == 0) {
       
-        printf("forked\n");
+        args[0] = "cp";
+        args[1] = "Intro.wmv";
+        args[2] = "test1.wav";
+        args[3] = NULL;
+        execvp("cp", args);
+        exit(1);
+      
+      }
+      
+      pid = fork();
+      
+      if (pid < 0)
+        exit(1);
+      
+      if (pid == 0) {
+      
+        args[0] = "cp";
+        args[1] = "Intro.wmv";
+        args[2] = "test2.wav";
+        args[3] = NULL;
+        execvp("cp", args);
+        exit(1);
+      
+      }
+
+	    //conectare la server
+	    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+         	if (serverfd < 0) 
+            	exit(1);
+	    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+	    serv_addr.sin_family = AF_INET;
+	    serv_addr.sin_port = htons(portno);
+	    inet_aton(argv[1], &serv_addr.sin_addr);
+	    if (connect(serverfd,(struct sockaddr*) &serv_addr,sizeof(serv_addr)) < 0) 
+		    exit(1);
+        
+      //set FD_SET
+      FD_ZERO(&read_fds);
+      FD_ZERO(&tmp_fds);
+      FD_SET(0, &read_fds);
+      FD_SET(serverfd, &read_fds);
+      fdmax = serverfd;
+      
+      send(serverfd, &streamno, sizeof(int), 0);
+      
+      f_num = (f_num + 1) % 2;
+      
+      pid = fork();
+      
+      if (pid < 0)
+        exit(1);
+        
+      tmp_fds = read_fds;
+      
+      if (pid == 0) {
+      
         args[0] = "/bin/bash";
         args[1] = "play.sh";
-        args[2] = NULL;
+        args[2] = "&";
+        args[3] = NULL;
         execvp("/bin/bash", args);
-        printf("err\n");
         exit(1);
       
       }
 	    
 	    while (1) {
-	    
+          
 	      //receive file from server
         if ((k = recv(serverfd, buf, sizeof(long), 0)) <= 0) {
-		      printf("S-a inchis serverul vietii!!!!\n");
           exit(1);
         }
           
--- Client/play.sh
+++ Client/play.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-mplayer -fixed-vo gl -playlist play_list -loop 0
+mplayer -fixed-vo gl -playlist play_list -loop 0 2&> /dev/null
